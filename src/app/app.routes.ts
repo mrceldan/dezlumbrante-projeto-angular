cddde009868630b86d79dcde25212e3b09b3bbae
mrceldan <mahsouza8@icloud.com>
2025-10-22 11:26:29 -0300

    implementação da capa inicial e inicio do botão de roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router'; 
+import { SiteComponent } from '../site/site.component';
+import { HomeComponent } from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', component: SiteComponent, title: 'Dezlumbrante Capa' },
+  { path: 'home', component: HomeComponent, title: 'Dezlumbrante Loja' },
+  { path: '**', redirectTo: '' } 
+];
---
+++ src/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SiteComponent } from '../site/site.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
